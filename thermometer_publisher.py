540ddb1ce9d596fff9b4e9099f17f34a59e5465f
Rhys Anderson <rhys.anderson@mail.utoronto.ca>
2021-03-02 11:19:55 -0500

    Completed the CTC ethernet scripts. Also added remote control of heaters, and combined CTC publisher into one file for both CTCs. Should still figure out how to control and log at the same time.

--- thermometer_publisher.py
+++ thermometer_publisher.py
@@ -5,31 +5,36 @@
 # pub_init time.sleep is < your client_init time.sleep, then your client
 # socket will read less data than
 
-import os
 import time
-os.chdir('/home/vuthalab/gdrive/code/edm_control/headers')
 from headers.zmq_server_socket import zmq_server_socket
-from headers.CTC100 import CTC100
+from headers.CTC100_ethernet import CTC100
 
-topic = "CTC100_1"                        # Change this to whatever device you're going to use.
+topic = "CTC_100"                        # Change this to whatever device you're going to use.
 port = 5551                             # If port is in use, enter a different 4 digit port number.
 
 delay_time = 0.5 #s, between measurements
 
-# initialize device
-thermometer = CTC100('/dev/ctc100_1')   # what the hell is the device address for the CTC controller?
-if thermometer is None:
-    print("No device was loaded.")
+# initialize devices
+thermometer_1 = CTC100('192.168.0.104')
+thermometer_2 = CTC100('192.168.0.107')
+
+if thermometer_1 is None:
+    print("Thermometer 1 was not loaded.")
+    if thermometer_2 is None:
+        print("Thermometer 2 was not loaded.")
     exit()
 
 # create a publisher for this topic and port
 publisher = zmq_server_socket(port, topic)
 counter = 0
-channel_names = ['saph','coll','bott hs','east hs']
+channel_names_1 = ['saph','coll','bott hs','cell']
+channel_names_2 = ['srb4k', 'srb45k', '45k plate', '4k plate']
 
 while True:
     try:
-        temperatures = [thermometer.read(channel_names[i]) for i in [0,1,2,3]]
+        temperatures_1 = [thermometer_1.read(channel_names_1[i]) for i in [0,1,2,3]]
+        temperatures_2 = [thermometer_2.read(channel_names_2[i]) for i in [0,1,2,3]]
+        temperatures = temperatures_1 + temperatures_2
         data_dict = {'temperatures' : temperatures}
         publisher.send(data_dict)
         time.sleep(delay_time)
