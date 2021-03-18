e9b2a95558c27c4c2a8ad1805c436da160dfc028
vuthalab <electric.atoms@gmail.com>
2021-03-18 17:45:45 -0400

    EDM OBJECT inherits attributes from IMG, FRG730, MFC, and Pulsetube. When there is an temperature publisher running, then the EDM object conflicts with the existing ctc100 objects

--- headers/EDM.py
+++ headers/EDM.py
@@ -3,8 +3,10 @@
 #methods to incorperate: take temperature, take presure liveplot, log data
 
 #TEST COMMENT
+#import os, sys
+#sys.path.append('C:/Users/camil/Documents/Github/EDM')
 import os, sys
-sys.path.append('C:/Users/camil/Documents/Github/EDM')
+sys.path.append("/home/vuthalab/gdrive/code/edm_control")
 from headers.zmq_server_socket import zmq_server_socket
 from headers.zmq_client_socket import zmq_client_socket
 from headers.FRG730_ion_gauge_header import FRG730
@@ -23,44 +25,52 @@ class EDM(PulseTube, MFC, CTC100, FRG730, zmq_client_socket, zmq_server_socket )
     def __init__(self):
         PulseTube.__init__(self, address= '192.168.0.101') # initiat pulse tube
         MFC.__init__(self)       #initiate MFC comunication
-
-
-    def get_temp(self, zmq_client_socket ): #method to get temperature data from pressure temp logger
-
-       #NEED TO MAKE SOCKET CREATION CONDITIONAL ON SOCKET EXISTANCE!!
-                #======== Recieve data====#
-                connection_settings_CTC100 = {'ip_addr': 'localhost',  # ip address
+        #information neccesary to establish connections to get data from pressure gauages/ctc1000 thermometers
+        connection_settings_CTC100 = {'ip_addr': 'localhost',  # ip address
                     'port': 5551,            # our open port
                     'topic': 'CTC_100'}       # device
-
-                temp_socket= zmq_client_socket(connection_settings_CTC100) # create socket that can recieve information from already running publisher
-                temp_socket.make_connection() # connect socket to publisher i.e. let the socket hear what is going on
-                #=========Request Temperature Values ====#
-
-
-                temperatures1 = temp_socket.read_on_demand()[1]['temperatures']
-
-                return temperatures1
-    def get_presure(self, zmq_client_socket): #method to get pressure from temp_pressure logger
-        #NEED TO MAKE SOCKET CREATION CONDITION ON SOCKET EXISTANCE
         connection_settings_agilent = {'ip_addr': 'localhost',  # ip address
                        'port': 5553,            # our open port
                        'topic': 'FRG730'}       # device
         connection_settings_hornet = {'ip_addr': 'localhost',  # ip address
                         'port': 5550,            # our open port
                         'topic': 'IGM401'}       # device
+        #create conections to sockets. use.SOCKET_NAME.read_on_demand to get info
+        # I think, having the self. _____ creats an attribute ??
+        self.temp_socket = zmq_client_socket(connection_settings_CTC100) # create socket that can recieve information from already running publisher
+        self.temp_socket.make_connection() # connect socket to publisher i.e. let the socket hear what is going on
+        self.agilent_socket = zmq_client_socket(connection_settings_agilent) #create socket to hear agilient presure data
+        self.agilent_socket.make_connection()
+        self.hornet_socket = zmq_client_socket(connection_settings_hornet)#create socket to hear hornet data
+        self.hornet_socket.make_connection()
+      #  self.ctc100_1 = CTC100('192.168.0.104')
+       # self.ctc100_2= CTC100('192.168.0.107')
+        # attempt to make ctc100 attributeed
 
-            agilent_socket = zmq_client_socket(connection_settings_agilent) #create socket to hear agilient presure data
-            agilent_socket.make_connection()
-            hornet_socket = zmq_client_socket(connection_settings_hornet)#create socket to hear hornet data
-            hornet_socket.make_connection()
+
+
+    def get_temp(self): #method to get temperature data from pressure temp logger
+
+       #NEED TO MAKE SOCKET CREATION CONDITIONAL ON SOCKET EXISTANCE!!
+                #======== Recieve data====#
+
+                #=========Request Temperature Values ====#
+                a = 10
+                temperatures1 = self.temp_socket.read_on_demand()
+                print(a)
+
+    def get_presure(self, zmq_client_socket): #method to get pressure from temp_pressure logger
+        #NEED TO MAKE SOCKET CREATION CONDITION ON SOCKET EXISTANCE
+
+
+
             #======Request Pressure Values====#
-            presure_agilent = agilent_socket.read_on_demand()[1]['pressure']
-            presure_hornet = hornet_socket.read_on_demand(1)[1]['pressure']
+        presure_agilent = agilent_socket.read_on_demand()[1]['pressure']
+        presure_hornet = hornet_socket.read_on_demand(1)[1]['pressure']
             #===== Concatanate Pressure Data ====#
-            presure_data = presure_agilent + presure_hornet
+        presure_data = presure_agilent + presure_hornet
 
-            return presure_data
+        return presure_data
 
 
     #currently,  The edm class can fully control the MFCs and the pulsetube. NEED TO TEST DATA PRESSURE DATA AND TEMP DATA REQUEST.
