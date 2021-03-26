0be5b33821db9a7d106cdf418c5c3f5f664a6b96
RhysAnderson <rhys.anderson@mail.utoronto.ca>
2021-03-26 18:21:33 -0400

    Had accidentally forgotten to save all changes to sequence.py. Committing and pushing.

--- sequence.py
+++ sequence.py
@@ -1,3 +1,6 @@
+#Import modules
+import time
+
 #Import class objects
 from headers.FRG730 import FRG730
 from headers.CTC100 import CTC100
@@ -7,6 +10,7 @@ from headers.zmq_server_socket import zmq_server_socket
 
 #Parameters
 delay_time = 0.05 #Time between measurements in seconds
+publish_time = 60.0 #Time to publish the data in seconds
 port = 5550 #Port for the publisher to broadcast on
 topic = 'EDM_monitor' #Topic broadcast via the publisher
 
@@ -27,8 +31,24 @@ T1.thermometer_names = ['saph','coll','bott hs','cell']
 T1.heater_names = ['heat saph', 'heat coll']
 T2.thermometer_names = ['srb4k', 'srb45k', '45k plate', '4k plate']
 T2.heater_names = ['srb45k out', 'srb4k out']
+L1.active_channels = ['AIN1','AIN2']
 
-#Start publishing the data
-for device in device_list:
-    
+#Start publishing the data for a specified amount of time
+start_time = time.time()
+while (time.time - start_time) < publish_time:
+    pressures = P1.read_pressure_torr()
+    time.sleep(delay_time)
+    temperatures = [T1.read(channel_names) for channel_names in T1.thermometer_names] + [T2.read(channel_name) for channel_names in T2.thermometer_names]
+    time.sleep(delay_time)
+    heater_outputs = [T1.read(channel_names) for channel_names in T1.heater_names] + [T2.read(channel_name) for channel_names in T2.heater_names]
+    time.sleep(delay_time)
+    monitor_voltages = [L1.read(channel_names) for channel_names in L1.active_channels]
+    time.sleep(delay_time)
+    mfc_flows = [Mfc.get_flow_rate_cell(), Mfc.get_flow_rate_neon_line()]
+    time.sleep(delay_time)
+    data_dict = {'pressures' : pressures, 'temperatures' : temperatures, 'heaters' : heater_outputs, 'voltages' : monitor_voltages, 'flows' : mfc_flows}
+    publisher.send(data_dict)
+    print(data_dict)
+
+
 
