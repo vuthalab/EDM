d062f6a63b1607e0e827a27b5acb4e377593109f
Samuel Li <contact@samuelj.li>
2021-05-05 15:07:09 -0400

    Attempt to read oscilloscope

--- interface/oscilloscope.py
+++ interface/oscilloscope.py
@@ -1,5 +1,7 @@
 from interface.usbtmc import USBTMC 
 
 class Oscilloscope(USBTMC):
-    """USBMTC interface for 
-    pass
+    """USBMTC interface for DS1102e oscilloscope."""
+
+    def __init__(self):
+        super().__init__('/dev/usbtmc0')
---
+++ read_oscilloscope.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+from interface.oscilloscope import Oscilloscope
+
+scope = Oscilloscope()
+scope.stop()
