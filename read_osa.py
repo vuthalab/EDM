7ae39bc79a0d9409205fdc42e75f7acad21282ae
Samuel Li <contact@samuelj.li>
2021-05-05 11:36:31 -0400

    Encapsulate OSA class

--- read_osa.py
+++ read_osa.py
@@ -3,76 +3,81 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# OSA Port + Address
-SERIAL_PORT = '/dev/ttyUSB2' # Run dmesg | grep "FTDI" to locate port
-GPIB_ADDRESS = 1 # Configurable on the OSA
+class OSA:
+    # Current serial connection
+    _ser = None
+
+    def __init__(self, serial_port, gpib_addr: int, baut: int = 19200):
+        # Open the serial connection
+        print(f'Opening serial connection on {port}...', end=' ', flush=True)
+        self._ser = serial.Serial(port, baud, timeout=2)
+        self._ser.reset_input_buffer()
+        self._ser.reset_output_buffer()
+        print('Done.')
+
+        # Set address and timeout
+        print('Bridge Version:', self.query('++ver'))
+        self.send_command(f'++addr {gpib_addr}')
+        self.send_command('++read_tmo_ms 30')
+
+        # Clear existing output
+        self.send_command('++read')
+
+        # Query OSA model
+        model = self.query('*idn?')
+        print('OSA Model:', model)
+
+    def stop():
+        """Closes the serial connection."""
+        self._ser.close()
 
 
-# Current serial connection
-ser = None
+    ##### Utility Functions #####
+    def send_command(command: str) -> None:
+        """Send a command to the GPIB device."""
+        self._ser.write((command + '\n').encode('utf-8'))
+        time.sleep(0.5)
 
-def open_connection(port: str, gpib_addr: int):
-    """Opens a new serial connection to the specified GPIB device."""
-    global ser
+    def query(command: str) -> str:
+        """Send a command to the GPIB device, and return its response."""
+        self.send_command(command)
+        response = self._ser.readline().strip()
+        return response.decode('utf-8')
 
-    # Open the serial connection
-    print(f'Opening serial connection on {port}...', end=' ', flush=True)
-    baud = 19200
-    ser = serial.Serial(port, baud, timeout=2)
-    ser.reset_input_buffer()
-    ser.reset_output_buffer()
-    print('Done.')
-
-    # Set address and timeout
-    print('Bridge Version:', query('++ver'))
-    send_command(f'++addr {gpib_addr}')
-    send_command('++read_tmo_ms 30')
-
-    # Clear existing output
-    send_command('++read')
-
-    # Query OSA model
-    model = query('*idn?')
-    print('OSA Model:', model)
+    def read_array(command: str):
+        """Read a float array from the GPIB device."""
+        entries = self.query(command).split(',')
+        n = int(entries[0])
+        data = np.array([float(x.strip()) for x in entries[1:]])
+        assert len(data) == n
+        return data
 
 
-##### Utility Functions #####
-def send_command(command):
-    """Send a command to the GPIB device."""
-    ser.write((command + '\n').encode('utf-8'))
-    time.sleep(0.5)
+    ##### Data Reading #####
+    def get_wavelengths(trace: str):
+        """Retrieves the wavelength data (nm) from the specified trace."""
+        print('Retrieving wavelengths (nm)...')
+        return self.read_array(f'WDAT{trace}')
 
-def query(command):
-    """Send a command to the GPIB device, and return its response."""
-    send_command(command)
-    response = ser.readline().strip()
-    return response.decode('utf-8')
+    def get_levels(trace: str):
+        """Retrieves the level data (dBm) from the specified trace."""
+        print('Retrieving levels (dBm)...')
+        return self.read_array(f'LDAT{trace}')
 
-def read_array(command):
-    """Read a float array from the GPIB device."""
-    entries = query(command).split(',')
-    n = int(entries[0])
-    data = np.array([float(x.strip()) for x in entries[1:]])
-    assert len(data) == n
-    return data
-
-
-def get_spectrum(trace: str):
-    """Retrieves the specified trace from the OSA."""
-    # Retrieve data
-    print('Retrieving wavelengths (nm)...')
-    wavelengths = read_array(f'WDAT{trace}')
-
-    print('Retrieving levels (dBm)...')
-    levels = read_array(f'LDAT{trace}')
-    return wavelengths, levels
+    def get_spectrum(trace: str):
+        """Retrieves the specified trace from the OSA."""
+        return self.get_wavelengths(trace), self.get_levels(trace)
 
 
 if __name__ == '__main__':
-    open_connection(SERIAL_PORT, GPIB_ADDRESS)
+    # OSA Port + Address
+    SERIAL_PORT = '/dev/ttyUSB2' # Run dmesg | grep "FTDI" to locate port
+    GPIB_ADDRESS = 1 # Configurable on the OSA
+
+    osa = OSA(SERIAL_PORT, GPIB_ADDRESS)
 
     # Get sample spectrum from OSA
-    wavelengths, levels = get_spectrum('A')
+    wavelengths, levels = osa.get_spectrum('A')
 
     # Plot data
     plt.plot(wavelengths, levels, label=f'Trace {trace}')
@@ -81,4 +86,4 @@ if __name__ == '__main__':
     plt.title(f'Ambient Spectrum')
     plt.show()
 
-    ser.close()
+    osa.stop()
