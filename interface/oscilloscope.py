980c61f739ee8ea9a6c4900bc0a664b7e106d1c1
vuthalab <electric.atoms@gmail.com>
2021-05-06 11:45:38 -0400

    Dump changes

--- interface/oscilloscope.py
+++ interface/oscilloscope.py
@@ -4,4 +4,4 @@ class Oscilloscope(USBTMC):
     """USBMTC interface for DS1102e oscilloscope."""
 
     def __init__(self):
-        super().__init__('/dev/usbtmc0')
+        super().__init__('/dev/usbtmc1')
